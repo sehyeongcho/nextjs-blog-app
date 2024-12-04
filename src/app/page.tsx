63b033286e819b9fe3dd51072ae9e8410621655d
sehyeongcho <shcho4701@gmail.com>
2024-12-04 17:09:13 +0900

    feat: update page.tsx (list posts)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import homeStyles from "../styles/Home.module.css";
+import { GetStaticProps } from "next";
+import { getSortedPostsData } from "@/lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }: {
+  allPostsData: {
+    id: string
+    date: string
+    title: string
+  }[]
+}) {
   return (
     <div>
       <Head>
@@ -14,9 +22,27 @@ export default function Home() {
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
         <ul className={homeStyles.list}>
-
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <a>{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>
+                {date}
+              </small>
+            </li>
+          ))}
         </ul>
       </section>
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
