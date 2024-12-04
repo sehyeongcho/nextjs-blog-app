107274c814045c2c3fa5f46ab5ce9771d83bdf2f
sehyeongcho <shcho4701@gmail.com>
2024-12-04 16:22:24 +0900

    docs: add comments on posts.ts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -7,12 +7,15 @@ console.log("process.cwd()", process.cwd());
 console.log("postsDirectory", postsDirectory);
 
 export function getSortedPostsData() {
+  // Reads the contents of the directory. (src/posts)
   const fileNames = fs.readdirSync(postsDirectory);
   console.log("fileNames", fileNames);
 
   const allPostsData = fileNames.map(fileName => {
     const id = fileName.replace(/\.md$/, "");
     const fullPath = path.join(postsDirectory, fileName);
+
+    // Synchronously reads the entire contents of a file.
     const fileContents = fs.readFileSync(fullPath, "utf8");
     const matterResult = matter(fileContents);
 
