14ea3440b6b2dfc1d88b13805a1875c8a9543ba5
sehyeongcho <shcho4701@gmail.com>
2024-12-05 20:42:00 +0900

    feat: update page.tsx (generateStaticParams)

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,7 +1,26 @@
-export default function Post() {
+import { getAllPostIds, getPostData } from "@/lib/posts"
+
+export default async function Post({ params }: { params: { id: string } }) {
+  console.log('params', params)
+  
+  const postData: {
+      title: string
+      date: string
+      contentHtml: string
+  } = await getPostData(params.id as string)
+
   return (
     <div>
-
+      <title>{postData.title}</title>
     </div>
   )
+}
+
+export function generateStaticParams() {
+  const paths = getAllPostIds()
+  console.log('paths', paths)
+
+  return paths.map((path: { id: string }) => ({
+    id: path.id
+  }))
 }
