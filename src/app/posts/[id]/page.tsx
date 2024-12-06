8a351900922cea490ef02e6d0a97d3c9f6949ba3
sehyeongcho <shcho4701@gmail.com>
2024-12-06 15:09:24 +0900

    docs: add comments on page.tsx

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getAllPostIds, getPostData } from "@/lib/posts"
 import Head from "next/head"
 
+// `generateStaticParams`가 반환한 `params`를 사용하여 이 페이지의 여러 버전이 정적으로 생성됩니다.
 export default async function Post({ params }: { params: { id: string } }) {
   console.log('params', params)
 
@@ -26,10 +27,12 @@ export default async function Post({ params }: { params: { id: string } }) {
   )
 }
 
+// `generateStaticParams` 함수는 동적 경로 세그먼트와 함께 사용되어 요청 시점이 아닌 빌드 시점에 경로를 정적으로 생성합니다.
 export function generateStaticParams() {
   const paths = getAllPostIds()
   console.log('paths', paths)
 
+  // [id] 동적 세그먼트를 채우기 위한 'params' 목록을 반환합니다.
   return paths.map((path: { id: string }) => ({
     id: path.id
   }))
