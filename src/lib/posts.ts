fdbb949d7eecf5b66119aa1716a97f57a704a588
sehyeongcho <shcho4701@gmail.com>
2024-12-07 01:32:30 +0900

    docs: add comments on posts.ts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -9,14 +9,14 @@ import rehypeStringify from "rehype-stringify";
 const postsDirectory = path.join(process.cwd(), "src\\posts");
 
 export function getSortedPostsData() {
-  // Reads the contents of the directory. (src/posts)
+  // 디렉토리의 내용을 읽습니다. (src/posts)
   const fileNames = fs.readdirSync(postsDirectory);
 
   const allPostsData = fileNames.map(fileName => {
     const id = fileName.replace(/\.md$/, "");
     const fullPath = path.join(postsDirectory, fileName);
 
-    // Synchronously reads the entire contents of a file.
+    // 파일의 전체 내용을 동기적으로 읽습니다.
     const fileContents = fs.readFileSync(fullPath, "utf8");
     const matterResult = matter(fileContents);
 
@@ -51,9 +51,9 @@ export async function getPostData(id: string) {
   const matterResult = matter(fileContents)
 
   const processedContent = await remark()
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
-    .use(rehypeStringify)
+    .use(remarkRehype, { allowDangerousHtml: true }) // HTML 태그 허용
+    .use(rehypeRaw) // HTML 태그를 파싱
+    .use(rehypeStringify) // HTML로 변환
     .process(matterResult.content)
 
   const contentHtml = processedContent.toString()
