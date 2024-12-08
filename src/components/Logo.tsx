db7b3cb53ce140e7d51adff1d93f3248e5e65d3d
sehyeongcho <shcho4701@gmail.com>
2024-12-08 21:41:09 +0900

    design: create Logo.tsx

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export default function Logo() {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/32b21ed9-cf81-4229-8793-c497da17f430/BCO4m9RpDh.lottie"
+      loop
+      autoplay
+    />
+  )
+}
